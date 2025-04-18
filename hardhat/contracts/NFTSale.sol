d869501d63086d5651bd862c4a2d7549552ce8f6
Shasika Udayanga <shasikaudayanga@gmail.com>
2025-04-18 19:10:59 +1000

    feat: mods

--- hardhat/contracts/NFTSale.sol
+++ hardhat/contracts/NFTSale.sol
@@ -10,64 +10,78 @@ import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Rec
 contract NFTSale is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
 
     IERC721 public premiumCollection;
+    uint256 public premiumNFTCollectionSize;
     IERC721 public standardCollection;
-    uint256 public premiumNFTId;
-    uint256 public standardNFTId;
+    uint256 public standardNFTCollectionSize;
     uint256 public constant PREMIUM_PRICE = 0.02 ether;
     uint256 public constant STANDARD_PRICE = 0.01 ether;
+
+    event PremiumNFTPurchased(address indexed buyer, uint256 tokenId);
+    event StandardNFTPurchased(address indexed buyer, uint256 tokenId);
 
     constructor(address initialOwner) 
         Ownable(initialOwner)
     {}
 
-
-    function setPremiumCollection(address _premiumCollection, uint256 tokenId) external onlyOwner {
-        premiumCollection = IERC721(_premiumCollection);
-        premiumNFTId = tokenId;
+    function setPremiumCollection(address collection, uint256 collectionSize) external onlyOwner {
+        premiumCollection = IERC721(collection);
+        premiumNFTCollectionSize = collectionSize;
     }
 
-    function setStandardCollection(address _standardCollection, uint256 tokenId) external onlyOwner {
-        standardCollection = IERC721(_standardCollection);
-        standardNFTId = tokenId;
+    function setStandardCollection(address collection, uint256 collectionSize) external onlyOwner {
+        standardCollection = IERC721(collection);
+        standardNFTCollectionSize = collectionSize;
+    }
+
+    function getCurrentPremiumNFTId() public view returns (uint256) {
+        for (uint256 i = 0; i < premiumNFTCollectionSize; i++) {
+            if (premiumCollection.ownerOf(i) == address(this)) {
+                return i;
+            }
+        }
+        revert("No available premium NFT");
+    }
+
+    function getCurrentStandardNFTId() public view returns (uint256) {
+        for (uint256 i = 0; i < standardNFTCollectionSize; i++) {
+            if (standardCollection.ownerOf(i) == address(this)) {
+                return i;
+            }
+        }
+        revert("No available standard NFT");
     }
 
     function buyPremiumNFT() external payable whenNotPaused nonReentrant {
         require(msg.value == PREMIUM_PRICE, "Incorrect Ether sent");
-        require(address(premiumCollection) != address(0), "Premium collection not set");
-        require(premiumCollection.ownerOf(premiumNFTId) == address(this), "NFT not available for sale");
-        premiumCollection.safeTransferFrom(address(this), msg.sender, premiumNFTId);
-        premiumNFTId = premiumNFTId + 1;
+        uint256 nftId = getCurrentPremiumNFTId();
+        premiumCollection.safeTransferFrom(address(this), msg.sender, nftId);
+        emit PremiumNFTPurchased(msg.sender, nftId);
     }
 
     function buyStandardNFT() external payable whenNotPaused nonReentrant {
         require(msg.value == STANDARD_PRICE, "Incorrect Ether sent");
-        require(address(standardCollection) != address(0), "Standard collection not set");
-        require(standardCollection.ownerOf(standardNFTId) == address(this), "NFT not available for sale");
-        standardCollection.safeTransferFrom(address(this), msg.sender, standardNFTId);
-        standardNFTId = standardNFTId + 1;
+        uint256 nftId = getCurrentStandardNFTId();
+        standardCollection.safeTransferFrom(address(this), msg.sender, nftId);
+        emit StandardNFTPurchased(msg.sender, nftId);
     }
 
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
-    function setPremiumNFTId(uint256 tokenId) external onlyOwner {
-        premiumNFTId = tokenId;
-    }
-
-    function setStandardNFTId(uint256 tokenId) external onlyOwner {
-        standardNFTId = tokenId;
-    }
-
     function withdrawNFT(address collection, uint256 tokenId) external onlyOwner {
         IERC721(collection).safeTransferFrom(address(this), owner(), tokenId);
     }
 
-    function withdrawAllNFTs(address collection, uint256 collectionSize) external onlyOwner {
-        for (uint256 i = 0; i < collectionSize; i++) {
-            address ownerOfNft = IERC721(collection).ownerOf(i);
-            if (ownerOfNft == address(this)) {
-                IERC721(collection).safeTransferFrom(address(this), owner(), i);
+    function withdrawAllNFTs() external onlyOwner {
+        for (uint256 i = 0; i < premiumNFTCollectionSize; i++) {
+            if (premiumCollection.ownerOf(i) == address(this)) {
+                premiumCollection.safeTransferFrom(address(this), owner(), i);
+            }
+        }
+        for (uint256 i = 0; i < standardNFTCollectionSize; i++) {
+            if (standardCollection.ownerOf(i) == address(this)) {
+                standardCollection.safeTransferFrom(address(this), owner(), i);
             }
         }
     }
